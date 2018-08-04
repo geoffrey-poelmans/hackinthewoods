d7ee1cd1cb69fb7d5eb244e36d94a62f22340f2b
Geoffrey <geoffrey.poelmans@gmail.com>
2018-08-04 18:39:05 +0200

    changement assiette

--- main.js
+++ main.js
@@ -1,67 +1,88 @@
-// OBJECT
-var boeufBio = {image:"boeuf", protein:"", legume:"", feculent:""};
-var boeufIndu = {image:"boeuf", protein:"", legume:"", feculent:""};
-var boeufMiddle = {image:"boeuf", protein:"", legume:"", feculent:""};
-var pouletBio = {image:"poulet", protein:"", legume:"", feculent:""};
-var pouletMiddle = {image:"poulet", protein:"", legume:"", feculent:""};
-var saladeBio = {image:"salade", protein:"", legume:"", feculent:""};
-var saladeIndu = {image:"salade", protein:"", legume:"", feculent:""};
-var saladeMiddle = {image:"salade", protein:"", legume:"", feculent:""};
-var tomateBio = {image:"tomate", protein:"", legume:"", feculent:""};
-var tomateMiddle = {image:"tomate", protein:"", legume:"", feculent:""};
-var patateBio = {image:"patate", protein:"", legume:"", feculent:""};
-var patateIndu = {image:"patate", protein:"", legume:"", feculent:""};
-var patateMiddle = {image:"patate", protein:"", legume:"", feculent:""};
-var burger = {imageBurger:"burger",imageSalade:"salade",imagefrites:"frites", protein:"", legume:"", feculent:""};
-var legume = {image:"legume", protein:"", legume:"", feculent:""};
-var rizBio = {image:"riz", protein:"", legume:"", feculent:""};
-var rizMiddle = {image:"riz", protein:"", legume:"", feculent:""};
-
 // VARIABLES
-var multiplicateur = $("#multiplicateur").text();
-var viande = $("#viande").text();
-var feculent = $("#feculent").text();
-var legume = $("#legume").text();
+// var multiplicateur = $("#multiplicateur").text();
+var position1 = $("#position1").text();
+var position2 = $("#position2").text();
+var position3 = $("#position3").text();
+
+// var barLegume = ajusteBar($("#barLegume").width());
+// var barViande = ajusteBar($("#barViande").width());
+// var barFeculent = ajusteBar($("#barFeculent").width());
+//
+// $("lol1").append(barLegume);
+// $("lol2").text(barViande);
+// $("lol3").text(barFeculent);
+//
+// console.log(barLegume);
+// console.log(barViande);
+// console.log(barFeculent);
 
 //FILTRE
 var echelleDeGris = 100;
 $("img").css("filter", "grayscale("+ echelleDeGris +"%)");
 
-
-//FONCTIONS PLUS
+//FONCTION PLUS
 $("#plus").click(function(){
   plus();
 });
-
 function plus() {
-  multiplicateur++;
-  $(".lol").css("display","block");
-  if (multiplicateur < 2) {
-    $("#multiplicateur").css("display","none");
-  } else {
-    $("#multiplicateur").css("display","block");
-    $("#multiplicateur").text(multiplicateur);
+  if ($('#position-1').attr("src") == '') {
+    $("#position-1").attr("src","http://www.orseu-concours.com/181-318-thickbox/test-epso-de-raisonnement-numerique-3-fr.jpg");
+  } else if ($('#position-2').attr("src") == '') {
+    $("#position-2").attr("src","https://files.brightside.me/files/news/part_44/445710/preview-19411360-650x341-98-1517840572.jpg");
+  } else if ($('#position-3').attr("src") == '') {
+    $("#position-3").attr("src","http://www.orseu-concours.com/181-318-thickbox/test-epso-de-raisonnement-numerique-3-fr.jpg");
   }
-  
 }
-
-
-//FONCTIONS MOINS
-$("#moins").click(function() {
-  moins();
-});
 
 function moins() {
-    if (multiplicateur != 0) {
-      multiplicateur--;
-    }
-  if (multiplicateur < 1) {
-    $(".lol").css("display","none");
-  }
-  if (multiplicateur < 2) {
-    $("#multiplicateur").css("display","none");
-  } else {
-    $("#multiplicateur").css("display","block");
-    $("#multiplicateur").text(multiplicateur);
+  if ($('#position-3').attr("src") != '') {
+    $("#position-3").attr("src","");
+  } else if ($('#position-2').attr("src") != '') {
+    $("#position-2").attr("src","");
+  } else if ($('#position-1').attr("src") != '') {
+    $("#position-1").attr("src","");
   }
 }
+
+
+
+
+//TEST
+
+
+// //FONCTIONS PLUS
+// $("#plus").click(function(){
+//   plus();
+// });
+//
+// function plus() {
+//   multiplicateur++;
+//   $(".position-assiette").css("display","inline");
+//   if (multiplicateur < 2) {
+//     $("#multiplicateur").css("display","none");
+//   } else {
+//     $("#multiplicateur").css("display","block");
+//     $("#multiplicateur").text(multiplicateur);
+//   }
+// }
+//
+//
+// //FONCTIONS MOINS
+// $("#moins").click(function() {
+//   moins();
+// });
+//
+// function moins() {
+//     if (multiplicateur != 0) {
+//       multiplicateur--;
+//     }
+//   if (multiplicateur < 1) {
+//     $(".lol").css("display","none");
+//   }
+//   if (multiplicateur < 2) {
+//     $("#multiplicateur").css("display","none");
+//   } else {
+//     $("#multiplicateur").css("display","block");
+//     $("#multiplicateur").text(multiplicateur);
+//   }
+// }
